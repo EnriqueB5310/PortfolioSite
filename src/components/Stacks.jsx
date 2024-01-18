614b2c37d758c06b088f9a7680492cf79f345858
Enrique Barrios <EnriqueB5310@gmail.com>
2024-01-18 11:30:17 -0500

    finjshed stack section

--- src/components/Stacks.jsx
+++ src/components/Stacks.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from './Card'
 function Stacks() {
   return (
-    <div className='bg-slate-950'>
+    <div className='bg-slate-950 mb-20'>
 
 <h1 className='text-slate-50 pt-20 pb-10 text-2xl font-bold text-center'>My tech stack</h1>
 
@@ -24,22 +24,97 @@ function Stacks() {
         <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>React</h1>
         </div>
 
-        <div>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>Tailwind</h1>
+        <div className='flex'>
+          
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" className='w-10' />
+          
+        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5 ml-5'>Tailwind</h1>
         </div>
         </div>
         </div>
 
 
 
-        <div className='flex flex-row justify-center lg:ml-5 bg-slate-500 rounded-md lg:mt-0 mt-10 pl-20 pr-20'>
+        <div className='flex flex-row justify-center lg:ml-5 bg-slate-500 rounded-md pl-20 lg:mt-0 mt-5 pr-20'>
         <div className='flex-row'>
         <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>Back-end</h1>
-        <h1 className='text-slate-50 text-lg font-bold pt-5'>Node</h1>
-        <h1 className='text-slate-50 text-lg font-bold pt-5'>Express</h1>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>Spring</h1>
+
+        <div className='flex'>  
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg" className='w-10 '></img>
+        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>Node</h1>
+        </div>
+
+        <div className='flex'>
+            
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" className='w-10' />
+          
+        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>Express</h1>
+        </div>
+
+        <div className='flex'>
+          
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" className='w-10' />
+          
+        <h1 className='text-slate-50 text-lg font-bold pt-5  ml-5 pb-5'>Spring</h1>
         </div>
         </div>
+        </div>
+
+
+        
+        <div className='flex flex-row justify-center lg:ml-5 bg-slate-500 rounded-md pl-20 lg:mt-0 mt-5 pr-20 '>
+        <div className='flex-row'>
+        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>Databases</h1>
+
+        <div className='flex'>  
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" className='w-10 '></img>
+        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>MongoDB</h1>
+        </div>
+
+        <div className='flex'>
+            
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" className='w-10' />
+          
+        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>SQL</h1>
+        </div>
+
+        <div className='flex'>
+          
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" className='w-10' />
+          
+        <h1 className='text-slate-50 text-lg font-bold pt-5  ml-5 pb-5'>Postgres</h1>
+        </div>
+        </div>
+        </div>
+
+
+        <div className='flex flex-row justify-center lg:ml-5 bg-slate-500 rounded-md pl-20 lg:mt-0 mt-5 pr-20 min-h-5'>
+        <div className='flex-row'>
+        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>Tools</h1>
+
+        <div className='flex'>  
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" className='w-10 '></img>
+        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>GitHub</h1>
+        </div>
+
+        <div className='flex'>
+            
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" className='w-10' />
+          
+        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>VSC</h1>
+        </div>
+
+        <div className='flex'>
+          
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" className='w-10' />
+          
+        <h1 className='text-slate-50 text-lg font-bold pt-5  ml-5 pb-5'>AWS</h1>
+        </div>
+        </div>
+        </div>
+
+
+
         </div>
 </div>
     
