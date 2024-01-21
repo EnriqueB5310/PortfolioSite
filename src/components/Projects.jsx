5f61a4408cff455d87bfaf424d8607c1140537f8
Enrique Barrios <EnriqueB5310@gmail.com>
2024-01-21 17:28:38 -0500

    projects page added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+   
+   
+    <h1 className='text-slate-50 pt-10 pb-10 text-2xl font-bold text-center'>Projects</h1>
+
+
+
   )
 }
 
