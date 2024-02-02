80433a12534bda873691b41f2251ea35185576c0
Enrique Barrios <EnriqueB5310@gmail.com>
2024-02-01 19:05:57 -0500

    fixed text not wrapping

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function ProjectCard(props) {
   return (
-   <div className='bg-slate-500 px-4 flex  items-center flex-col text-center'>
+   <div className='bg-slate-500 px-4 flex  items-center flex-col justify-center text-center break-words lg:max-w-sm'>
 <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>{props.project}</h1>
-<p className='text-slate-50'>{props.bio}</p>
+<p className='text-slate-50 break-words text-pretty pb-8 whitespace-pre'>{props.bio}</p>
 
 <div className='flex w-10'>
 <img src={props.stack1} className='w-10' />
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
    <>
     <h1 className='text-slate-50 pt-10 pb-10 text-2xl font-bold text-center'>Projects</h1>
 
-    <div className='flex justify-center'>
+    <div className='flex justify-center content-center flex-row '>
 <ProjectCard
 project="Flight API"
 bio="A webAPI made in Java's Spring framework that handles the storage and mock purchases of flights"
