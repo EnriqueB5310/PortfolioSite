e2a3f6350cb8e7b89f23a68bb41ce36a1867f192
Enrique Barrios <EnriqueB5310@gmail.com>
2024-02-24 21:52:51 -0500

    stavk section fix

--- src/components/Stacks.jsx
+++ src/components/Stacks.jsx
@@ -1,124 +1,99 @@
-import React from 'react'
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+
 
 function Stacks() {
+  // Skills
+  const frontEnd = [
+    {
+      title: "JavaScript",
+      icon: <IoLogoJavascript size={50} />,
+    },
+    {
+      title: "React.js",
+      icon: <FaReact size={50} />,
+    },
+    {
+      title: "TailwindCSS",
+      icon: <SiTailwindcss size={50} />,
+    },
+  ];
+
+  const backEnd = [
+    {
+      title: "Node.JS",
+      icon: <FaNodeJs size={50} />,
+    },
+    {
+      title: "Express.JS",
+      icon: <SiExpress size={50} />,
+    },
+    {
+      title: "PostgreSQL",
+      icon: <BiLogoPostgresql size={50} />,
+    },
+  ];
+
   return (
-    <div className='bg-slate-800 mb-20'>
-
-<h1 className='text-slate-50 pt-20 pb-10 text-2xl font-bold text-center'>My Tech Stack</h1>
-
-<div className='flex flex-col  lg:flex-row lg:justify-center items-center pb-20'>
-
-<div className='flex flex-row justify-center lg:ml-5 bg-slate-500 rounded-md pl-20 pr-20'>
-        <div className='flex-row'>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>Front-end</h1>
-
-        <div className='flex'>  
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" className='w-10 '></img>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>JS</h1>
+    <section
+      id="skills"
+      className="flex flex-col items-center justify-center bg-secondary lg:min-h-screen mx-auto "
+    >
+     
+        {/* Title */}
+        <div className="mt-[6px]">
+          <h1 className="text-[60px] lg:text-[60px] 2xl:text-[80px] md:text-[50px] text-white font-bold">
+            Tech Stack
+          </h1>
         </div>
 
-        <div className='flex'>
-            
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" className='w-10' />
-          
-        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>React</h1>
-        </div>
+        {/* Boxes */}
+        <div className="mt-9 space-y-5 lg:space-y-0 flex flex-col items-center lg:flex-row lg:space-x-10 mb-[50px] 2xl:mb-[80px] ">
+          {/* Front-End Skills */}
+          <div className="w-[350px] h-[450px] bg-primary flex flex-col items-center justify-evenly rounded-[20px] shadow-lg transition duration-300 transform hover:shadow-2xl">
+            <h1 className="text-white text-4xl font-bold mb-5 ">Front-End</h1>
+            <ul>
+              {frontEnd.map((skill) => {
+                return (
+                  <li key={skill.title} className="flex items-center mb-3">
+                    <div className="text-white translate-colors duration-300 hover:text-bHoverColor">
+                      {skill.icon}
+                    </div>
+                    <span className="text-xl text-white ml-2">
+                      {skill.title}
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
 
-        <div className='flex'>
-          
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" className='w-10' />
-          
-        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5 ml-5'>Tailwind</h1>
+          {/* Back-End Skills */}
+          <div className="w-[350px] h-[450px] bg-primary flex flex-col items-center justify-evenly rounded-[20px] shadow-lg transition duration-300 transform hover:shadow-2xl">
+            <h1 className="text-white text-4xl font-bold mb-5">Back-End</h1>
+            <ul>
+              {backEnd.map((skill, index) => {
+                return (
+                  <li key={index} className="flex items-center mb-3">
+                    <div className="text-white translate-colors duration-300 hover:text-bHoverColor">
+                      {skill.icon}
+                    </div>
+                    <span className="text-xl text-white ml-2">
+                      {skill.title}
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-        </div>
-        </div>
-
-
-
-        <div className='flex flex-row justify-center lg:ml-5 bg-slate-500 rounded-md pl-20 lg:mt-0 mt-5 pr-20'>
-        <div className='flex-row'>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>Back-end</h1>
-
-        <div className='flex'>  
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg" className='w-10 '></img>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>Node</h1>
-        </div>
-
-        <div className='flex'>
-            
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" className='w-10' />
-          
-        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>Express</h1>
-        </div>
-
-        <div className='flex'>
-          
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" className='w-10' />
-          
-        <h1 className='text-slate-50 text-lg font-bold pt-5  ml-5 pb-5'>Spring</h1>
-        </div>
-        </div>
-        </div>
-
-
-        
-        <div className='flex flex-row justify-center lg:ml-5 bg-slate-500 rounded-md pl-20 lg:mt-0 mt-5 pr-20 '>
-        <div className='flex-row'>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>Databases</h1>
-
-        <div className='flex'>  
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" className='w-10 '></img>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>MongoDB</h1>
-        </div>
-
-        <div className='flex'>
-            
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" className='w-10' />
-          
-        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>SQL</h1>
-        </div>
-
-        <div className='flex'>
-          
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" className='w-10' />
-          
-        <h1 className='text-slate-50 text-lg font-bold pt-5  ml-5 pb-5'>Postgres</h1>
-        </div>
-        </div>
-        </div>
-
-
-        <div className='flex flex-row justify-center lg:ml-5 bg-slate-500 rounded-md pl-20 lg:mt-0 mt-5 pr-20 min-h-5'>
-        <div className='flex-row'>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>Tools</h1>
-
-        <div className='flex'>  
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" className='w-10 '></img>
-        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>GitHub</h1>
-        </div>
-
-        <div className='flex'>
-            
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" className='w-10' />
-          
-        <h1 className='text-slate-50 text-lg font-bold pt-5 ml-5'>VSC</h1>
-        </div>
-
-        <div className='flex'>
-          
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" className='w-10' />
-          
-        <h1 className='text-slate-50 text-lg font-bold pt-5  ml-5 pb-5'>AWS</h1>
-        </div>
-        </div>
-        </div>
-
-
-
-        </div>
-</div>
-    
-  )
+ 
+    </section>
+  );
 }
 
 export default Stacks
