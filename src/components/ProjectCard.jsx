b7738d023733f9d03dc928bd19131d79fa3c6026
Enrique <EnriqueB5310@gmail.com>
2024-02-26 13:06:32 -0500

    fixes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,17 +2,33 @@ import React from 'react'
 
 function ProjectCard(props) {
   return (
-   <div className='bg-slate-700 px-4 flex  items-center flex-col justify-center text-center break-words lg:max-w-sm'>
-<h1 className='text-slate-50 text-lg font-bold pt-5 pb-5'>{props.project}</h1>
-<p className='text-slate-50 break-words text-pretty pb-8 whitespace-pre'>{props.bio}</p>
+    <div className="container mx-auto py-[50px]">
+      <div className="flex flex-col items-center justify-center">
+        {/* Cards */}
+        <div className="bg-secondary w-[380px] h-[320px] rounded-[20px] lg:w-[1000px] lg:h-[400px] shadow-lg transition duration-300 transform hover:shadow-2xl filter hover:brightness-90">
+          <div className="flex flex-col lg:flex-row text-white text-center">
+            {/* Image */}
+            <div className="lg:px-5 lg:py-[50px] ">
+              
+            </div>
+</div>
+<div className="flex flex-col justify-center lg:pr-4 text-white">
+              {/* Title */}
+              <div className="font-bold text-primaryTextColor text-[30px] lg:mt-[0px] mt-[30px]">
+                <h1>Test</h1>
+              </div>
 
-<div className='flex w-10'>
-<img src={props.stack1} className='w-10' />
-          
+              {/* Description */}
+              <div className="mt-[10px] text-[20px]">
+                <p>Test</p>
+              </div>
+              </div>
+</div>
 </div>
 
-</div>
-  )
+     </div>       
+   
+  );
 }
 
 export default ProjectCard
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,13 +21,12 @@ tech: null,
    <section id="projects" className=" min-h-[910px] bg-primary">
       
         <div className="font-bold pt-[90px] text-[60px] text-white flex justify-center text-center ">
-          <h1>Projects</h1>
+          <h1 className='mb-20 hover:text-violet-600 hover:duration-500'>Projects</h1>
         </div>
         <div className="container lg:flex lg:justify-center mx-auto">
          <ProjectCard
          
-         project="Mp3 Converter"
-         bio="Converts youtube videos into mp3 or mp4 format for offline use"
+  
          
          />
         </div>
